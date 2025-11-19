7f4a1866d82dfcfab70dc239bd697713edf30b27
`Faiz-js` <faiz.momin18112003@gmail.com>
2025-11-19 14:23:00 +0530

    Trust section completed

--- src/app/components/LandingPage/LandingPage.tsx
+++ src/app/components/LandingPage/LandingPage.tsx
@@ -1,11 +1,13 @@
 import Hero from "../Hero/Hero";
 import Navbar from "../Navbar/Navbar";
+import Trust from "../Trust/Trust";
 
 export default function LandingPageComponent() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <Trust />
     </main>
   );
 }
