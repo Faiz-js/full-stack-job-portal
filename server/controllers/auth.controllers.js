76a6bd8a1eeec56a1a74b20ab684deb625d061d0
`Faiz-js` <faiz.momin18112003@gmail.com>
2026-02-04 20:31:37 +0530

    removed mongodb part

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,36 +1,35 @@
-const User = require("../models/User.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const signup = async (req, res) => {
   try {
-    const { fullname, email, mobileNumber, password } = req.body;
+  //   const { fullname, email, mobileNumber, password } = req.body;
 
-    if (!fullname || !email || !mobileNumber || !password) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Please fill all fields" });
-    }
+  //   if (!fullname || !email || !mobileNumber || !password) {
+  //     return res
+  //       .status(400)
+  //       .json({ success: false, message: "Please fill all fields" });
+  //   }
 
-    const isExisting = await User.findOne({
-      $or: [{ email }, { mobileNumber }],
-    });
+  //   const isExisting = await User.findOne({
+  //     $or: [{ email }, { mobileNumber }],
+  //   });
 
-    if (isExisting) {
-      return res.status(409).json({
-        success: false,
-        message: "User with these email or mobile number already exists",
-      });
-    }
+  //   if (isExisting) {
+  //     return res.status(409).json({
+  //       success: false,
+  //       message: "User with these email or mobile number already exists",
+  //     });
+  //   }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+  //   const hashedPassword = await bcrypt.hash(password, 10);
 
-    await User.create({
-      fullname,
-      email,
-      mobileNumber,
-      password: hashedPassword,
-    });
+  //   await User.create({
+  //     fullname,
+  //     email,
+  //     mobileNumber,
+  //     password: hashedPassword,
+  //   });
 
     return res
       .status(200)
@@ -48,37 +47,37 @@ const signup = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
+  // const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please fill all fields" });
-  }
+  // if (!email || !password) {
+  //   return res
+  //     .status(400)
+  //     .json({ success: false, message: "Please fill all fields" });
+  // }
 
-  const userInDb = await User.findOne({ email });
+  // const userInDb = await User.findOne({ email });
 
-  if (!userInDb) {
-    return res.status(404).json({ success: false, message: "No such user" });
-  }
+  // if (!userInDb) {
+  //   return res.status(404).json({ success: false, message: "No such user" });
+  // }
 
-  const isPassMatch = await bcrypt.compare(password, userInDb.password);
+  // const isPassMatch = await bcrypt.compare(password, userInDb.password);
 
-  if (!isPassMatch) {
-    return res.status(400).json({ success: false, message: "No such user" });
-  }
+  // if (!isPassMatch) {
+  //   return res.status(400).json({ success: false, message: "No such user" });
+  // }
 
-  const token = jwt.sign(
-    { userID: userInDb._id, email: userInDb.email },
-    process.env.JWT_SECRET,
-    { expiresIn: "24h" }
-  );
+  // const token = jwt.sign(
+  //   { userID: userInDb._id, email: userInDb.email },
+  //   process.env.JWT_SECRET,
+  //   { expiresIn: "24h" }
+  // );
 
-  res.cookie("token", token, {
-    httpOnly: true,
-    sameSite: "lax",
-    maxAge: 24 * 60 * 60 * 1000,
-  });
+  // res.cookie("token", token, {
+  //   httpOnly: true,
+  //   sameSite: "lax",
+  //   maxAge: 24 * 60 * 60 * 1000,
+  // });
 
   return res.status(200).json({ success: true, message: "Login successful!" });
 };
--- server/index.js
+++ server/index.js
@@ -2,11 +2,9 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
-const connectDb = require("./config/dbConnection.js");
 const authRoutes = require("./routes/auth.routes.js");
 
 dotenv.config();
-connectDb();
 
 const PORT = process.env.PORT || 5000;
 
